09b50c73f1fc3dc7e86026cdfc79539352327487
Tyrannanas <129948998+SimonBinzenbach@users.noreply.github.com>
2023-12-11 16:28:18 +0100

    first steps
    added preprocess function to flatten out data matrixes
    added ReLU(X) as variable

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ import tensorflow_datasets as tfds
 
 matplotlib.rcParams['figure.figsize'] = [9, 6]
 tf.random.set_seed(42)
+
+# importiertes dataset
+train_data, val_data, test_data = tfds.load("mnist",
+                                            split=['train[10000:]', 'train[0:10000]', 'test'],
+                                            batch_size=128, as_supervised=True)
+
+
+def preprocess(x, y):  # function for flattening out feature matrix
+    x = tf.reshape(x, shape=[-1, 784])
+    x = x / 255
+    return x, y
+
+
+train_data, val_data = train_data.map(preprocess), val_data.map(preprocess) # using preprocess funtion to flatten out data matrixes
+
+x = tf.linspace(-2, 2, 201) # ReLU(X)
+x = tf.cast(x, tf.float32)
+
+
+plt.show()
