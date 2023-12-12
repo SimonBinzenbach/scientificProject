82daa10228551d9c2d7d8e7570de9fc5e59dd089
Tyrannanas <129948998+SimonBinzenbach@users.noreply.github.com>
2023-12-12 16:34:14 +0100

    added functions
    2 Optimization funtions
    loss function
    acurracy function
    initialized mlp
    next -> training loop
    
    keep in mind that the loss function will be updated if we swap to binary output

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def preprocess(x, y):  # function for flattening out feature matrix
 
 
 train_data, val_data = train_data.map(preprocess), val_data.map(
-    preprocess)  # using preprocess funtion to flatten out data matrixes
+    preprocess)  # using preprocess funtion to flatten out data matrices
 
 # activation functions
 reLU_ls = tf.linspace(-2, 2, 15)  # ReLU(X)
@@ -44,13 +44,11 @@ sigmoid = tf.nn.sigmoid(sigmoid_ls)
 tanh_ls = tf.linspace(-5, 5, 15)  # Tanh
 tanh_ls = tf.cast(tanh_ls, tf.float32)
 tanh = tf.nn.tanh(tanh_ls)
-
-
 # plt.plot(tanh_ls, tanh)
 
 
-# Denselayer
-class DenseLayer(tf.Module):
+# MLP Initialization stuff
+class DenseLayer(tf.Module):  # Function to initialize DenseLayer
     def __init__(self, out_dim, weight_init=tf.keras.initializers.GlorotUniform(), activation=tf.identity):
         super().__init__()
         self.out_dim = out_dim
@@ -68,8 +66,9 @@ class DenseLayer(tf.Module):
         return self.activation(z)
 
 
-class MLP(tf.Module):
+class MLP(tf.Module):  # Function to initialize MLP
     def __init__(self, layers):
+        super().__init__()
         self.layers = layers
 
     @tf.function
@@ -78,5 +77,71 @@ class MLP(tf.Module):
             x = layer(x)
         return x
 
+
+# Optimizers
+class GradientDescent(tf.Module):  # GradientDescent
+    def __init__(self, learning_rate=1e-3):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.title = f"Gradient descent optimizer: learning rate={self.learning_rate}"
+
+    def apply_gradients(self, grads, vars):
+        # Update variables
+        for grad, var in zip(grads, vars):
+            var.assign_sub(self.learning_rate * grad)
+
+
+class Adam(tf.Module):  # Adaptive moment estimation
+
+    def __init__(self, learning_rate=1e-3, beta_1=0.9, beta_2=0.999, ep=1e-7):
+        super().__init__()
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.learning_rate = learning_rate
+        self.ep = ep
+        self.t = 1.
+        self.v_dvar, self.s_dvar = [], []
+        self.title = f"Adam: learning rate={self.learning_rate}"
+        self.built = False
+
+    def apply_gradients(self, grads, vars):
+        if not self.built:
+            for var in vars:
+                v = tf.Variable(tf.zeros(shape=var.shape))
+                s = tf.Variable(tf.zeros(shape=var.shape))
+                self.v_dvar.append(v)
+                self.s_dvar.append(s)
+            self.built = True
+        for i, (d_var, var) in enumerate(zip(grads, vars)):
+            self.v_dvar[i] = self.beta_1 * self.v_dvar[i] + (1 - self.beta_1) * d_var
+            self.s_dvar[i] = self.beta_2 * self.s_dvar[i] + (1 - self.beta_2) * tf.square(d_var)
+            v_dvar_bc = self.v_dvar[i] / (1 - (self.beta_1 ** self.t))
+            s_dvar_bc = self.s_dvar[i] / (1 - (self.beta_2 ** self.t))
+            var.assign_sub(self.learning_rate * (v_dvar_bc / (tf.sqrt(s_dvar_bc) + self.ep)))
+        self.t += 1.
+
+
+def cross_entropy_loss(y_pred, y):  # cross entropy loss function
+    sparse_ce = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=y_pred)
+    return tf.reduce_mean(sparse_ce)
+
+
+def accuracy(y_pred, y):  # accuracy function
+    class_preds = tf.argmax(tf.nn.softmax(y_pred), axis=1)
+    is_equal = tf.equal(y, class_preds)
+    return tf.reduce_mean(tf.cast(is_equal, tf.float32))
+
+
+# Initialization of MLP
+hidden_layer_1_size = 700
+hidden_layer_2_size = 500
+output_size = 10
+mlp_model = MLP([
+    DenseLayer(out_dim=hidden_layer_1_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=hidden_layer_2_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=output_size)])
+
+
+# Training Loop
 
 plt.show()
