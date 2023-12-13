a16b5614cde2f168495122a7288937ce3f4ac9be
Oliver/SquidSushi_ <35111378+SquidSushi@users.noreply.github.com>
2023-12-13 16:22:38 +0100

    moved verticaledgemap into main
    my head hurts

--- imagefilters.py
+++
@@ -1,9 +0,0 @@
-# Description: This file contains the functions that are used to create the feature maps
-def verticalEdgeMap(image):
-    # create a matrix of zeros with the size of the image
-    matrix = np.zeros((image.shape[0], image.shape[1]))
-    for x in range(0, image.shape[0]):
-        for y in range(0, image.shape[1] - 1):
-            matrix[x][y] = np.max(image[x, y]) - np.max(image[x, y + 1])
-    return matrix
-
--- main.py
+++ main.py
@@ -7,6 +7,16 @@ import os
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+
+def verticalEdgeMap(image):
+    # create a matrix of zeros with the size of the image
+    matrix = np.zeros((image.shape[0], image.shape[1]))
+    for x in range(0, image.shape[0]):
+        for y in range(0, image.shape[1] - 1):
+            matrix[x][y] = np.max(image[x, y]) - np.max(image[x, y + 1])
+    return matrix
+
+
 matplotlib.rcParams['figure.figsize'] = [9, 6]
 tf.random.set_seed(42)
 
